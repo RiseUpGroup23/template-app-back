5ba1deaf56015b6f4f01e50b7671f89c9ca2c38a
alejoguzmann <alejoguzmann20@gmail.com>
2024-07-03 14:42:05 -0300

    listo wp, se lo  pide en el webhook de mp? o pantalla succes de mp

--- routes/wp.js
+++ routes/wp.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 const router = express.Router();
 
 //pal .env
-const whatsappToken =
-  "EAARcGh1d8gsBO1IsC9RlzOZA1UN2fBbeMdopjtlf9nbGIrgzZCPNYQfgGZCSoH6hOPMmW8YpkD6u31NGDdUZBcLyAvREJb2AHIxbEDOE6ezdhlh6H6MPGTvj8IzBFvukTDHOAlqGmIlZC59I41ZCYEZBcdMf9aLV9yZBQafqPgYoxZBzZCHmYyBjtWZAHwLJzVXUnO8tcAI3kT8djU1DKsE0zwZD";
-const whatsappPhoneNumberId = "386041251250531";
+const whatsappToken = //dura 24hs
+  "EAARcGh1d8gsBO4V3j7lzEI3a9yCK5wdCJ8ooEcSQ2CBnwedKkwo2TkZBEcqNv2nTyVZCybfw0lmM0l8ZBrHmDsGDxAHM3fPm1B41sEPQUJjNg8xGDcJ7i8cpnJfFqareV9CKxgo5Mf4UEQZAVdGwfbO5M8EZBxgRZAvczTiEkAfrZCo2MLsnznnAV78RZArhztYXYyRqV05wfZAs1FUp4TYUZD";
+const whatsappPhoneNumberId = "386041251250531"; //id numero dueño
 
 router.post("/wp", async (req, res) => {
   try {
@@ -13,9 +13,41 @@ router.post("/wp", async (req, res) => {
       `https://graph.facebook.com/v11.0/${whatsappPhoneNumberId}/messages`,
       {
         messaging_product: "whatsapp",
-        to: "543815729513",
+        to: "543815729513", //req.body.phone
         type: "template",
-        template: { name: "hello_world", language: { code: "en_US" } },
+        template: {
+          name: "turno",
+          language: {
+            code: "es",
+          },
+          components: [
+            {
+              type: "body",
+              parameters: [
+                {
+                  type: "text",
+                  text: "Alejo", //req.body.appointment.customer.name
+                },
+                {
+                  type: "text",
+                  text: "03-07-2024", //req.body.apponintment.date
+                },
+                {
+                  type: "text",
+                  text: "18:00", //req.body.apponintment.time
+                },
+                {
+                  type: "text",
+                  text: "Avenida Aconquija 150", //config?
+                },
+                {
+                  type: "text",
+                  text: "BarberShop", //config?
+                },
+              ],
+            },
+          ],
+        },
       },
       {
         headers: {
