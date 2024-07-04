5e59915c3eb671f34b791cb772505bbbda3c9193
Nicolás Amicone <nicolas@pocimadigital.com>
2024-07-04 17:02:42 -0300

    Model

--- routes/mp.js
+++ routes/mp.js
@@ -42,6 +42,7 @@ function transformarObjeto(objeto) {
 // POST
 router.post("/mercadopago/crear-preferencia", async (req, res) => {
   try {
+    console.log("crear pref", req.body.appointment);
     const body = {
       items: [
         {
@@ -77,27 +78,24 @@ router.post("/mercadopago/crear-preferencia", async (req, res) => {
 router.post("/webhook", async (req, res) => {
   let paymentQ = req.query;
   //try {
-    if (paymentQ.type === "payment") {
-      const result = await payment.get({
-        id: paymentQ["data.id"],
-      });
-      console.log("solo metadata",result.metadata);
-      console.log("tranform metadata",transformarObjeto(result.metadata));
-      const existingAppointment = await Appointment.findOne({
-        date: result.metadata.date,
-        disabled: false,
-      });
-      if (result.status === "approved" && !existingAppointment) {
-        axios.post(
-          "https://template-peluquerias-back.vercel.app/appointments"
-        ),
-          transformarObjeto(result.metadata);
-      }
-      return res.status(200);
+  if (paymentQ.type === "payment") {
+    const result = await payment.get({
+      id: paymentQ["data.id"],
+    });
+    console.log("solo metadata", result.metadata);
+    console.log("tranform metadata", transformarObjeto(result.metadata));
+    const existingAppointment = await Appointment.findOne({
+      date: result.metadata.date,
+      disabled: false,
+    });
+    if (result.status === "approved" && !existingAppointment) {
+      axios.post("https://template-peluquerias-back.vercel.app/appointments", transformarObjeto(result.metadata))
     }
+    return res.status(200);
+  }
   //} catch (error) {
-    //console.error(error);
-    //return res.status(500).json({ error: error.message });
+  //console.error(error);
+  //return res.status(500).json({ error: error.message });
   //}
 });
 
