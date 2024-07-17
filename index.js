180817c2cc7c7b419bf8945caabede33b095f383
Nicolás Amicone <nicolas@pocimadigital.com>
2024-07-17 11:36:03 -0300

    entorno

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
